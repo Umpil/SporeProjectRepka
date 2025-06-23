80e23c65c3848af66c028acef51fb617927498d2
Umpil <157904322+Umpil@users.noreply.github.com>
2025-06-23 12:20:17 +0300

    Update serianumber.py

--- Utils/serianumber.py
+++ Utils/serianumber.py
@@ -1,13 +1,5 @@
 repka_id = 0
-username = "Admin"
-password = "123"
-url = "https://spore.k-lab.su/"
-url_check = "https://spore.k-lab.su/check"
-url_upload_image = "https://spore.k-lab.su/upload/image"
-url_upload_log = "https://spore.k-lab.su/upload/log"
-url_upload_logimage = "https://spore.k-lab.su/upload/logimage"
-url_update_log = "https://spore.k-lab.su/update/log"
-url_upload_add_image = "https://spore.k-lab.su/upload/add/photo"
+
 # resp = requests.get(url, {"rust": "15", "much": "23", "pirena": "22", "type_": "send"})
 #
 # print(resp.request)
